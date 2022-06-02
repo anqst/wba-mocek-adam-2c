78373ffba67019aa3750ac96b3758f82b3fd1b68
mocekadam <mocek.ad.2020@skola.ssps.cz>
2022-06-02 11:28:14 +0200

    ez xd

--- javascript-eyes/scripts/index.js
+++ javascript-eyes/scripts/index.js
@@ -1,4 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    console.log("run");
+    const open = document.querySelector("#open");
+    const hidden = document.querySelector("#hidden");
+
+    hidden.style.display = "none";
+
+    open.addEventListener("click", function(){
+
+        if(hidden.style.display == "none"){
+
+        hidden.style.display = "flex";
+        console.log(hidden.parentElement.offsetTop, hidden.offsetHeight);
+        hidden.style.top = `${-hidden.offsetHeight + open.offsetHeight}px`;
+        hidden.style.top = `${open.offsetHeight}px`;
+
+        } else {
+
+            hidden.style.top = `${-hidden.offsetHeight + open.offsetHeight}px`;
+            let trans;
+            hidden.addEventListener("transitionend", trans = function(){
+
+                hidden.style.display = "none";
+                hidden.removeEventListener("transitionend", trans);
+            });
+        }
+    });
+
+
+    // key capture
+    window.addEventListener("keydown", function(e){
+
+        console.log(e.key);
+    });
 });
